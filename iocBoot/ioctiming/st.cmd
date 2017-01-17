1cb314e62202c2b2525b8bd8535648fd852c12de
andrei_pereira <andrei.pereira@lnls.br>
2017-01-17 10:57:40 -0200

    Adequação de todos os nomes de PV na convenção Sirius
    Convenção: Sec-Sub:Dis-Dev[-Idx]:PROP[.FIELD]
    
    Parte inicial dos nomes implementada "Sec-Sub:Dis-Dev[-Idx]:". Resta adequação dos campos PROP.
    
    ===================================
    
    !!! [NOTA ESPECIAL] StreamDevice => O StreamDevice possui um 'bug' que limita consideravelmente o tamanho válido de PVs quando se faz usode redirecionamento para records. !CAUSANDO BUFFER OVERFLOW! quando o nome é maior que o permitido.
    
    O arquivo de protocolo faz uso extenso de redirecionamento para records neste IOC. Foi necessário, portanto, reduzir o tamanho do nome de alguns records, principalmente dos records cmd_, que em geral possuem os maiores nomes.
    Foi notado também que strings passadas como parâmetro para o protocolo "ocupam mais espaço", levando em conta a limitação mencionada, que caracteres escritos diretamente no protocolo em um campo de redirecionamento. Aparentemente, o dobro do espaço.
    
    Um problema muito similar é discutido em: http://www.aps.anl.gov/epics/tech-talk/2015/msg00720.php
    
    E uma possível solução : http://www.aps.anl.gov/epics/tech-talk/2015/msg00721.php
    ====================================
    
    >> Modificações no IOC:
    
    	-Mudança dos prefixos nos arquivos .db.
    	-Redução de nomes grandes de record (principalmente records 'cmd_').
    	-Modificação dos prefixos nos arquivos da SEQUENCER .stt.
    	-Modificação nas linhas de código que faziam uso da macro com o prefixo dos records nos arquivos .stt.
    	-Alteração dos prefixos nas OPIs do CS-Studio.

--- iocBoot/ioctiming/st.cmd
+++ iocBoot/ioctiming/st.cmd
@@ -32,34 +32,34 @@ drvAsynIPPortConfigure ("EVE2", "10.0.18.54:50122:50122 udp",0,0,0)
 ## Load record instances
 
 # EVG
-dbLoadRecords("${TOP}/db/evg.db", "device=EVG1, ITBL_LEN=2000, PORT=EVG1, ADDR=0, TIMEOUT=2")
-dbLoadRecords "${TOP}/db/SeqRAM.db", "device=EVG1"
+dbLoadRecords("${TOP}/db/evg.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, ITBL_LEN=2000, PORT=EVG1, ADDR=0, TIMEOUT=2")
+dbLoadRecords "${TOP}/db/SeqRAM.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1"
 
 # Events
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=0, desc='Diagnostic', code=0x02, time=100, mode=0, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=1, desc='Septa LTB', code=0x03, time=150, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=2, desc='Egun Trigger', code=0x04, time=160, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=3, desc='Kicker LTB', code=0x05, time=170, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=0, desc='Diagnostic', code=0x02, time=100, mode=0, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=1, desc='Septa LTB', code=0x03, time=150, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=2, desc='Egun Trigger', code=0x04, time=160, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=3, desc='Kicker LTB', code=0x05, time=170, mode=1, transm=0"
 
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=4, desc='Diagnostic', code=0x06, time=180, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=5, desc='Septa LTB', code=0x07, time=190, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=6, desc='Egun Trigger', code=0x08, time=200, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=7, desc='Kicker LTB', code=0x09, time=210, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=4, desc='Diagnostic', code=0x06, time=180, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=5, desc='Septa LTB', code=0x07, time=190, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=6, desc='Egun Trigger', code=0x08, time=200, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=7, desc='Kicker LTB', code=0x09, time=210, mode=1, transm=0"
 
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=8, desc='Diagnostic', code=0x0A, time=220, mode=1, transm=0"
-dbLoadRecords "${TOP}/db/Eventos.db", "device=EVG1, num=9, desc='Septa LTB', code=0x0B, time=230, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=8, desc='Diagnostic', code=0x0A, time=220, mode=1, transm=0"
+dbLoadRecords "${TOP}/db/Eventos.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, num=9, desc='Septa LTB', code=0x0B, time=230, mode=1, transm=0"
 
 # EVR
 #dbLoadRecords("${TOP}/db/evr.db", "device=EVR1, PORT=EVR1, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/evr.db", "device=EVR2, PORT=EVR2, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/evr.db", "device=EVR3, PORT=EVR3, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/evr.db", "device=EVR4, PORT=EVR4, ADDR=0, TIMEOUT=2")
-dbLoadRecords("${TOP}/db/evr.db", "device=EVR5, PORT=EVR5, ADDR=0, TIMEOUT=2")
+dbLoadRecords("${TOP}/db/evr.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVR, Idx=5, PORT=EVR5, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/evr.db", "device=EVR6, PORT=EVR6, ADDR=0, TIMEOUT=2")
 
 # EVE
-dbLoadRecords("${TOP}/db/eve.db", "device=EVE1, PORT=EVE1, ADDR=0, TIMEOUT=2")
-dbLoadRecords("${TOP}/db/eve.db", "device=EVE2, PORT=EVE2, ADDR=0, TIMEOUT=2")
+dbLoadRecords("${TOP}/db/eve.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVE, Idx=1, PORT=EVE1, ADDR=0, TIMEOUT=2")
+dbLoadRecords("${TOP}/db/eve.db", "Sec=AS, Sub=Inj, Dis=TI, Dev=EVE, Idx=2, PORT=EVE2, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/eve.db", "device=EVE3, PORT=EVE3, ADDR=0, TIMEOUT=2")
 #dbLoadRecords("${TOP}/db/eve.db", "device=EVE4, PORT=EVE4, ADDR=0, TIMEOUT=2")
 
@@ -72,20 +72,20 @@ iocInit
 ## Start any sequence programs
 
 # Module initialization and network status
-seq sncEVGSetup, "device=EVG1"
+seq sncEVGSetup, "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1"
 
 #seq sncEVRESetup, "device=EVR1"
 #seq sncEVRESetup, "device=EVR2"
 #seq sncEVRESetup, "device=EVR3"
 #seq sncEVRESetup, "device=EVR4"
-seq sncEVRESetup, "device=EVR5"
+seq sncEVRESetup, "Sec=AS, Sub=Inj, Dis=TI, Dev=EVR, Idx=5"
 #seq sncEVRESetup, "device=EVR6"
 
-seq sncEVRESetup, "device=EVE1"
-seq sncEVRESetup, "device=EVE2"
+seq sncEVRESetup, "Sec=AS, Sub=Inj, Dis=TI, Dev=EVE, Idx=1"
+seq sncEVRESetup, "Sec=AS, Sub=Inj, Dis=TI, Dev=EVE, Idx=2"
 #seq sncEVRESetup, "device=EVE3"
 #seq sncEVRESetup, "device=EVE4"
 
 # Injection System State Machine
-seq sncSeqRAM, "device=EVG1, ev_num=10"
+seq sncSeqRAM, "Sec=AS, Sub=Inj, Dis=TI, Dev=EVG, Idx=1, ev_num=10"
 
