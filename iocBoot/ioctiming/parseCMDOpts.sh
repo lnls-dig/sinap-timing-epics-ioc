51cff11f31618599a0a2d36113156629ab99af51
Joao Leandro de Brito Neto <joao.brito@lnls.br>
2017-12-12 17:31:46 -0200

    iocBoot: split st.cmd into st*.cmd/*.sh/*.config/ for better structuration
    Now, we have split the st.cmd into various files:
    
     - Config file, used for static environment variables;
     - Instrument-specific st.cmd, so it's easier to maintain;
     - Runnable wrapper script, as it's more convenient.

---
+++ iocBoot/ioctiming/parseCMDOpts.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        "-i"|"--ip-addr") IPADDR="$2" ;;
+        "-p"|"--ip-port") IPPORT="$2" ;;
+        "-x"|"--index") IDX="$2" ;;
+        "-d"|"--device-type") DEVICE="$2" ;;
+        *) echo "Usage:" >&2
+            echo "  $0 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL] [-RDMM RDMM_VAL] " >&2
+            echo >&2
+            echo " Options:" >&2
+            echo "  -i or --ip-addr     Configure IP address to connect to device" >&2
+            echo "  -p or --ip-port     Configure IP port number to connect to device" >&2
+            echo "  -x or --index       Configure device index number" >&2
+            echo "  -d or --device      Configure DMM device type [DCCT<number>|ICT<number>|DMM<number>]" >&2
+            echo "  -P                  Configure value of \$(P) macro" >&2
+            echo "  -R                  Configure value of \$(R) macro" >&2
+            echo "  -RDMM               Configure value of \$(RDMM) macro. Must be different from \$(R)" >&2
+            exit 2
+    esac
+
+    shift 2
+done
