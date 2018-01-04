3f008eb1cbe33752a6b5135a5db87f102af50f5d
Joao Leandro de Brito Neto <joao.brito@lnls.br>
2018-01-04 16:29:48 -0200

    Remove inavlid parameter option.

--- iocBoot/ioctiming/parseCMDOpts.sh
+++ iocBoot/ioctiming/parseCMDOpts.sh
@@ -8,12 +8,12 @@ while [ "$#" -gt 0 ]; do
         "-p"|"--ip-port") IPPORT="$2" ;;
         "-d"|"--device-type") DEVICE="$2" ;;
         *) echo "Usage:" >&2
-            echo "  $0 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL] [-RDMM RDMM_VAL] " >&2
+            echo "  $0 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL] " >&2
             echo >&2
             echo " Options:" >&2
             echo "  -i or --ip-addr     Configure IP address to connect to device" >&2
             echo "  -p or --ip-port     Configure IP port number to connect to device" >&2
-            echo "  -d or --device      Configure DMM device type [EVG<number>|EVR<number>|EVE<number>|FOUT<number>]" >&2
+            echo "  -d or --device      Configure Sinap Timing device type [EVG<number>|EVR<number>|EVE<number>|FOUT<number>]" >&2
             echo "  -P                  Configure value of \$(P) macro" >&2
             echo "  -R                  Configure value of \$(R) macro" >&2
             exit 2
