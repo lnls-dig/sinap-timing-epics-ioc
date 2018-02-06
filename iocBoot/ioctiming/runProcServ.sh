a4d61f2eaeb08f1588d985d11304ab8b948f16f6
Lucas Russo <lerwys@gmail.com>
2018-02-05 23:15:44 -0200

    iocBoot/*/runProcServ.sh: use specified TELNET port
    if not set, use the previous 20000 default.

--- iocBoot/ioctiming/runProcServ.sh
+++ iocBoot/ioctiming/runProcServ.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Use defaults is not set
+if [ -z "${TIMING_DEVICE_TELNET_PORT}" ]; then
+    TIMING_DEVICE_TELNET_PORT=20000
+fi
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n TIMING -i ^C^D 20000 ./runGenericModule.sh "$@"
+/usr/local/bin/procServ -f -n timing${RSFSV_INSTANCE} -i ^C^D ${RSFSV_DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
