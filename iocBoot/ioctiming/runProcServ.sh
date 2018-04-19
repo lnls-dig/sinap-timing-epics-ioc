dc590d61aa70c25479c138270d3a0301fe56bd4e
jlmais <joao.brito@lnls.br>
2018-04-19 16:09:19 -0300

    runProcServ.sh: fix wrong telnet/instance environment variables names

--- iocBoot/ioctiming/runProcServ.sh
+++ iocBoot/ioctiming/runProcServ.sh
@@ -6,4 +6,4 @@ if [ -z "${TIMING_DEVICE_TELNET_PORT}" ]; then
 fi
 
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n timing${RSFSV_INSTANCE} -i ^C^D ${RSFSV_DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
+/usr/local/bin/procServ -f -n timing${TIMING_INSTANCE} -i ^C^D ${TIMING_DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
