6c6150b1a4f9e65e5d9d45f9c50afb90ee54071b
Lucas Russo <lerwys@gmail.com>
2018-05-08 10:30:24 -0300

    iocBoot/*/runProcServ.sh: use parseCMDOpts script to parse options
    We just check for the variables we are interested
    in. So, many variables in this script can be empty.

--- iocBoot/ioctiming/runProcServ.sh
+++ iocBoot/ioctiming/runProcServ.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-# Use defaults is not set
-if [ -z "${TIMING_DEVICE_TELNET_PORT}" ]; then
-    TIMING_DEVICE_TELNET_PORT=20000
+set -e
+set +u
+
+# Parse command-line options
+. ./parseCMDOpts.sh "$@"
+
+# Use defaults if not set
+if [ -z "${DEVICE_TELNET_PORT}" ]; then
+   DEVICE_TELNET_PORT="20000"
 fi
 
+if [ -z "${TIMING_INSTANCE}" ]; then
+   TIMING_INSTANCE="EVE1"
+fi
+
+set -u
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n timing${TIMING_INSTANCE} -i ^C^D ${TIMING_DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
+/usr/local/bin/procServ -f -n TIMING_${TIMING_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
