cc9e8c9be01f81510262300ac97935293f282828
Lucas Russo <lerwys@gmail.com>
2018-05-15 18:43:58 -0300

    iocBoot/*/runProcServ.sh: fix wrong TIMING_INSTANCE variable
    The correct variable parsed by parseCMDOpts.sh is
    now called DEVICE_TYPE

--- iocBoot/ioctiming/runProcServ.sh
+++ iocBoot/ioctiming/runProcServ.sh
@@ -11,11 +11,11 @@ if [ -z "${DEVICE_TELNET_PORT}" ]; then
    DEVICE_TELNET_PORT="20000"
 fi
 
-if [ -z "${TIMING_INSTANCE}" ]; then
-   TIMING_INSTANCE="EVE1"
+if [ -z "${DEVICE_TYPE}" ]; then
+   DEVICE_TYPE="EVE1"
 fi
 
 set -u
 
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n TIMING_${TIMING_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
+/usr/local/bin/procServ -f -n TIMING_${DEVICE_TYPE} -i ^C^D ${DEVICE_TELNET_PORT} ./runGenericModule.sh "$@"
