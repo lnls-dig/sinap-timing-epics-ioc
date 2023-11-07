80fc686be093a120035b60bb8da43a0dfd6e56ba
Mauricio Donatti <mmdonatti@gmail.com>
2023-11-07 17:09:19 -0300

    scripts: add script to check firmware version

---
+++ scripts/ioc/sinap_list.py
@@ -0,0 +1,25 @@
+#sinap devices list
+devices = {
+  'EVG':['AS-RaMO:TI-EVG'],
+  'Fout':['RA-RaMO:TI-Fout',
+    'CA-RaTim:TI-Fout-1',
+    'CA-RaTim:TI-Fout-2',
+    'CA-RaTim:TI-Fout-3',
+    'CA-RaTim:TI-Fout-4',
+    'CA-RaTim:TI-Fout-5'],
+  'EVR':['LA-RFH01RACK2:TI-EVR',
+    'IA-01RaInj05:TI-EVR',
+    'LA-RaCtrl:TI-EVR',
+    'PA-RaCtrl:TI-EVR-1',
+    'PA-RaCtrl:TI-EVR-2',
+    'IA-20RaDiag01:TI-EVR',
+    'BA-IMBA:TI-EVR'],
+  'EVE':['LA-BIH01RACK2:TI-EVE',
+    'IA-16RaBbB:TI-EVE',
+    'IA-18RaDiag04:TI-EVE',
+    'IA-14RaDiag03:TI-EVE',
+    'BA-MNCA:TI-EVE',
+    'BA-CAXA:TI-EVE',
+    'RA-RaBO01:TI-EVE',
+    'RA-RaSIA01:TI-EVE']
+}
