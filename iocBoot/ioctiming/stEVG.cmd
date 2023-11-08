bee2143ebf1de2dfea60188620b9c458f67065a0
Érico Nogueira <erico.rolim@lnls.br>
2023-11-08 12:51:55 -0300

    instantiate rx_locked.db for EVG and Fout

--- iocBoot/ioctiming/stEVG.cmd
+++ iocBoot/ioctiming/stEVG.cmd
@@ -29,6 +29,8 @@ dbLoadRecords "${TOP}/db/SeqRAM.db", "P=${P}, R=${R}"
 dbLoadRecords("${TOP}/db/fw_version.db", "P=${P}, R=${R}, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 dbLoadRecords("${TOP}/db/event_log.db", "P=${P}, R=${R}, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 
+dbLoadRecords("${TOP}/db/rx_locked.db", "P=${P}, R=${R}, PORT=${PORT}, ADDR=0, TIMEOUT=2, DEVICE=evg")
+
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=0, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=1, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=2, PORT=${PORT}, ADDR=0, TIMEOUT=2")
