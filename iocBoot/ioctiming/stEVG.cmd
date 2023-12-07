2a15f9a8c40f7077c887c5b3970c6fbdc0b11486
Érico Nogueira <erico.rolim@lnls.br>
2023-12-07 16:37:27 -0300

    timingApp: limit how many bits of RxEnbl can be 1.
    The new hardware version still can't actually safely handle more than 4
    enabled Rx paths. This limitation used to be implemented in the gateware
    less transparently, but it was removed.

--- iocBoot/ioctiming/stEVG.cmd
+++ iocBoot/ioctiming/stEVG.cmd
@@ -32,6 +32,8 @@ dbLoadRecords("${TOP}/db/event_log.db", "P=${P}, R=${R}, PORT=${PORT}, ADDR=0, T
 
 dbLoadRecords("${TOP}/db/rx_locked.db", "P=${P}, R=${R}, PORT=${PORT}, ADDR=0, TIMEOUT=2, DEVICE=evg")
 
+dbLoadRecords("${TOP}/db/evgfout_rxenbl.db", "P=${P}, R=${R}")
+
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=0, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=1, PORT=${PORT}, ADDR=0, TIMEOUT=2")
 dbLoadRecords("${TOP}/db/evgfout_out.db", "P=${P}, R=${R}, num=2, PORT=${PORT}, ADDR=0, TIMEOUT=2")
